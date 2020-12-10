ebb2ac3c377ebe3d1531783702227ddb9839633a
Jonathan Vazquez <jonavaz18@gmail.com>
2020-12-10 16:11:32 -0600

    Lógica para calcular la cotización

--- App.js
+++ App.js
@@ -15,12 +15,26 @@ export default function App() {
   const [capital, setCapital] = useState(null);
   const [interes, setInteres] = useState(null);
   const [meses, setMeses] = useState(null);
+  const [total, setTotal] = useState(null);
 
   const calculate = () => {
-    console.log('capital->', capital);
-    console.log('interes->', interes);
-    console.log('meses->', meses);
+    if (!capital) {
+      console.log('Añade la cantidad que quieres ingresar');
+    } else if (!interes) {
+      console.log('Añade el interes del préstamo');
+    } else if (!meses) {
+      console.log('Selecciona los meses a pagar');
+    } else {
+      const i = interes / 100;
+      const fee = capital / ((1 - Math.pow(i + 1, -meses)) / i);
+
+      setTotal({
+        monthlyFee: fee.toFixed(2).replace('.', ','),
+        totalPayable: (fee * meses).toFixed(2).replace('.', ','),
+      });
+    }
   };
+  console.log(total);
   return (
     <>
       <SafeAreaView style={styles.safeArea}>
